916766d47b0bda0305782d53e17becdec346a054
ttpadvisory <bb@ttpadvisory.co.uk>
2025-05-12 16:00:58 +0100

    Update usefuldatefuncs.js
    Forgot about Date order so 12th May was coming is a 5th December - School Boy Error - Fixed

--- usefuldatefuncs.js
+++ usefuldatefuncs.js
@@ -1,2 +1,2 @@
 // Date with YYYY-MM-DD HH:MM:SS taking into account actual time with Daylight savings - Substiture 'en-GB' for desired coutry code
-const getDateTimeWithDLS = (countrycode='en-GB') => { return new Date(new Date().toLocaleString(countrycode)).toISOString().replace("T"," ").replace("Z","").substr(0,19) }
+const getDateTimeWithDLS = (countrycode='en-GB') => { return new Date().toISOString().substring(0,19) + ' ' + new Date().toTimeString().substring(0,8) }
