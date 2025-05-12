ed19b0f177fc2be07cd8eb6f2561a5e97bbf9c84
ttpadvisory <bb@ttpadvisory.co.uk>
2025-05-12 16:03:48 +0100

    Update usefuldatefuncs.js

--- usefuldatefuncs.js
+++ usefuldatefuncs.js
@@ -1,2 +1,2 @@
 // Date with YYYY-MM-DD HH:MM:SS taking into account actual time with Daylight savings - Substiture 'en-GB' for desired coutry code
-const getDateTimeWithDLS = (countrycode='en-GB') => { return new Date().toISOString().substring(0,19) + ' ' + new Date().toTimeString().substring(0,8) }
+const getDateTimeWithDLS = (countrycode='en-GB') => { return new Date().toISOString().substring(0,10) + ' ' + new Date().toTimeString().substring(0,8) }
